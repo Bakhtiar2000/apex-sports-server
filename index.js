b2245dd65e4274697f374179194466c6592dd51f
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-06-15 23:41:37 +0600

    added user get apis

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
         const classesCollection = client.db('apexSportsDb').collection('classes')
         const instructorsCollection = client.db('apexSportsDb').collection('instructors')
         const usersCollection = client.db('apexSportsDb').collection('users')
+        const selectionCollection = client.db('apexSportsDb').collection('selections')
 
         //Classes related api
         app.get('/classes', async (req, res) => {
@@ -49,12 +50,31 @@ async function run() {
         })
 
         //Users related api
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/users/:email', async (req, res) => {
+            console.log(req.params.email)
+            const query = { email: req.params.email }
+            const result = await usersCollection.findOne(query)
+            console.log(result, query)
+            res.send(result)
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body
             const result = await usersCollection.insertOne(user)
             res.send(result)
         })
 
+        //Selections related api
+        app.post('/selections', async (req, res) => {
+            const selected = req.body
+            const result = await selectionCollection.insertOne(selected)
+            res.send(result)
+          })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
