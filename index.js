69178490b0bcbb6c842ff66ddb8149bb4af0eb73
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-06-15 18:02:51 +0600

    added basic get operations for classes and indtructors

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-const app=express();
-const port= process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=> {
+
+
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvsxjlv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const classesCollection = client.db('apexSportsDb').collection('classes')
+        const instructorsCollection = client.db('apexSportsDb').collection('instructors')
+
+        //Classes api
+        app.get('/classes', async (req, res) => {
+            console.log(req.query.limit)
+            const limit = parseInt(req.query.limit);
+            const options = {
+                sort: { no_of_students: 1 },
+                limit:limit
+            };
+            const result = await classesCollection.find({},options).toArray()
+            res.send(result)
+        })
+
+        //Instructors api
+        app.get('/instructors', async (req, res) => {
+            console.log(req.query.limit)
+            const limit = parseInt(req.query.limit);
+            const result = await instructorsCollection.find().limit(limit).toArray()
+            res.send(result)
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally { }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Apex sports is busy playing')
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Apex sports server is running on port: ${port}`)
 })
