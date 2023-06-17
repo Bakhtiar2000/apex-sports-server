0f11b8e52d3cfc8e100b4a66986cab6aa3e42524
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-06-18 00:46:20 +0600

    added patch and put for classescollection

--- index.js
+++ index.js
@@ -29,49 +29,137 @@ async function run() {
         const instructorsCollection = client.db('apexSportsDb').collection('instructors')
         const usersCollection = client.db('apexSportsDb').collection('users')
         const selectionCollection = client.db('apexSportsDb').collection('selections')
-        const addedClassCollection = client.db('apexSportsDb').collection('addedClasses')
         const paymentCollection = client.db('apexSportsDb').collection('payments')
 
         //Classes related api
         app.get('/classes', async (req, res) => {
-            console.log(req.query.limit)
+            // console.log(req.query.email)
             const limit = parseInt(req.query.limit);
+            const email = req.query.email
+            const query = email ? { instructor_email: email } : {};
             const options = {
                 sort: { no_of_students: -1 },
                 limit: limit
             };
-            const result = await classesCollection.find({}, options).toArray()
+            // console.log(options)
+            const result = await classesCollection.find(query, options).toArray()
+            // console.log(result)
             res.send(result)
         })
-
+        //patch for setting class status
         app.patch('/classes/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id)
+            let updateDoc = {}
+            // console.log(id)
             const filter = { _id: new ObjectId(id) }
-            const update = { $inc: { available_seats: -1 } };
-            const result = await usersCollection.updateOne(filter, update)
+            const status = req.body.newStatus
+            const feedback = req.body.textareaValue
+            const updatedValues = req.body.updatedValues
+            const options = { upsert: true };
+
+            // console.log(updatedValues)
+            if (status) {
+                updateDoc = {
+                    $set: {
+                        status: status
+                    }
+                }
+            }
+            else if (feedback) {
+                updateDoc = {
+                    $set: {
+                        feedback: feedback
+                    }
+                }
+            }
+
+            else if (updatedValues) {
+                if(updatedValues.name =='' && updatedValues.price =='' && updatedValues.image ==''){
+                    updateDoc = {
+                        $set: {
+                            
+                        }
+                    }
+                }
+                else if(updatedValues.name =='' && updatedValues.price =='' ){
+                    updateDoc = {
+                        $set: {
+                            image: updatedValues.image
+                        }
+                    }
+                }
+                else if(updatedValues.price =='' && updatedValues.image ==''){
+                    updateDoc = {
+                        $set: {
+                            name: updatedValues.name
+                        }
+                    }
+                }
+                else if(updatedValues.name =='' && updatedValues.image ==''){
+                    updateDoc = {
+                        $set: {
+                            price: updatedValues.price
+                        }
+                    }
+                }
+                else if(updatedValues.name ==''){
+                    updateDoc = {
+                        $set: {
+                            price: updatedValues.price,
+                            image: updatedValues.image
+                        }
+                    }
+                }
+                else if(updatedValues.price ==''){
+                    updateDoc = {
+                        $set: {
+                            name: updatedValues.name,
+                            image: updatedValues.image
+                        }
+                    }
+                }
+                else if(updatedValues.image ==''){
+                    updateDoc = {
+                        $set: {
+                            name: updatedValues.name,
+                            price: updatedValues.price
+                        }
+                    }
+                }
+                else {
+                    updateDoc = {
+                        $set: {
+                            name: updatedValues.name,
+                            price: updatedValues.price,
+                            image: updatedValues.image,
+                        }
+                    }
+                }
+            }
+            
+            const result = await classesCollection.updateOne(filter, updateDoc, options)
             res.send(result)
         })
 
+        //put for increasing class available seats
+        app.put('/classes/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id)
+            const filter = { _id: new ObjectId(id) }
+            const update = { $inc: { no_of_students: 1, available_seats: -1 } };
+            const result = await classesCollection.updateOne(filter, update)
+            res.send(result)
+        })
 
-        //addedClasses related api
-        app.post('/addedClasses', async (req, res) => {
+        app.post('/classes', async (req, res) => {
             const addedClass = req.body;
-            // console.log(addedClass)
-            const result = await addedClassCollection.insertOne(addedClass)
-            res.send(result)
-        })
-
-        app.get('/addedClasses', async (req, res) => {
-            const email = req.query.email
-            const query = { instructor_email: email };
-            const result = await addedClassCollection.find(query).toArray()
+            const result = await classesCollection.insertOne(addedClass)
             res.send(result)
         })
 
         //Instructors related api
         app.get('/instructors', async (req, res) => {
-            console.log(req.query.limit)
+            // console.log(req.query.limit)
             const limit = parseInt(req.query.limit);
             const result = await instructorsCollection.find().limit(limit).toArray()
             res.send(result)
@@ -84,7 +172,7 @@ async function run() {
         })
 
         app.get('/users/:email', async (req, res) => {
-            // console.log(req.params.email)
+            console.log(req.params.email)
             const query = { email: req.params.email }
             const result = await usersCollection.findOne(query)
             res.send(result)
@@ -98,10 +186,10 @@ async function run() {
 
         app.patch('/users/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id)
+            // console.log(id)
             const filter = { _id: new ObjectId(id) }
             const updatedBooking = req.body
-            console.log(updatedBooking.newRole)
+            // console.log(updatedBooking.newRole)
             const updateDoc = {
                 $set: {
                     role: updatedBooking.newRole
@@ -114,13 +202,14 @@ async function run() {
         //Selections related api
         app.get('/selections', async (req, res) => {
             const email = req.query.email
-            console.log(email)
+            // console.log(email)
             const query = { email: email };
             const result = await selectionCollection.find(query).toArray()
             res.send(result)
         })
         app.delete('/selections/:id', async (req, res) => {
             const id = req.params.id;
+            // console.log(id)
             const query = { _id: new ObjectId(id) }
             const result = await selectionCollection.deleteOne(query)
             res.send(result)
@@ -137,7 +226,7 @@ async function run() {
         app.post('/create-payment-intent', async (req, res) => {
             const { price } = req.body
             const amount = parseInt(price * 100)
-            console.log(amount)
+            // console.log(amount)
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
@@ -152,18 +241,17 @@ async function run() {
         app.post('/payments', async (req, res) => {
             const payment = req.body
             const result = await paymentCollection.insertOne(payment)
-
             res.send(result)
         })
 
         app.get('/payments', async (req, res) => {
-            console.log(req.query.email)
+            // console.log(req.query.email)
             const email = req.query.email
             const query = { email: email }
-            const sortField = req.query.sort === 'newest' ? 'timestamp' : '';
-            const sort = sortField ? { [sortField]: -1 } : {};
-            console.log(sort)
-            const result = await paymentCollection.find(query).sort(sort).toArray()
+            const options = {
+                sort: { timestamp: 1 }
+            };
+            const result = await paymentCollection.find(query, options).toArray()
             res.send(result)
         })
 
